a1b70805c42b541f2dc8ff611c89f36d618cdb19
Beatrice-Beacco <beatrice.beacco@gmail.com>
2026-08-28 16:50:59 +0200

    feat: server llm model

--- agent-graph/llm.py
+++ agent-graph/llm.py
@@ -1,3 +1,17 @@
-from langchain_ollama import ChatOllama
+import os
 
-llm = ChatOllama(model="qwen3:8b", temperature=0, reasoning=False)
+from dotenv import load_dotenv
+from langchain_openai import ChatOpenAI
+from pydantic import SecretStr
+
+load_dotenv()
+
+llm_api_url = os.getenv("API_URL")
+llm_api_key = os.getenv("API_KEY")
+
+llm = ChatOpenAI(
+    model="vllm/qwen3.8-27b",
+    base_url=llm_api_url if llm_api_url is not None else None,
+    api_key=SecretStr(llm_api_key) if llm_api_key is not None else None,
+    temperature=0,
+)
